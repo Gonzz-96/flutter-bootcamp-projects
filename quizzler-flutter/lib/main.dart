e164ad2976baf925b8b862d89def818dbb09a83b
Gonzalo Campos <erick.campos@wizeline.com>
2020-04-18 17:45:29 -0500

    Create questions and add a stateful counter

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -25,6 +25,30 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+  List<String> listOfQuestions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  List<bool> listOfAnswers = [
+    false,
+    true,
+    true,
+  ];
+
+  int currentQuestion = 0;
+
+  void _incrementCurrentQuestionCounter() {
+    if (currentQuestion < listOfQuestions.length - 1) {
+      currentQuestion++;
+    } else {
+      currentQuestion = 0;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +61,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                listOfQuestions[currentQuestion],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +85,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  _incrementCurrentQuestionCounter();
+                });
               },
             ),
           ),
@@ -79,12 +105,17 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  _incrementCurrentQuestionCounter();
+                });
               },
             ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
