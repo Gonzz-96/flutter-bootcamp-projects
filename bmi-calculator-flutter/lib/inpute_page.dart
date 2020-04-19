c3de4e6000d505a02713b98994de200c17eb1861
Gonzalo Campos <erick.campos@wizeline.com>
2020-04-19 14:48:41 -0500

    Create resusable custom Widget and apply it

--- bmi-calculator-flutter/lib/inpute_page.dart
+++ bmi-calculator-flutter/lib/inpute_page.dart
@@ -12,12 +12,63 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              cardColor: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color cardColor;
+
+  ReusableCard({@required this.cardColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: cardColor,
       ),
     );
   }
