4775ce312d38a56b98ea8cf4e77fdf2f374c94aa
Gonzalo Campos <erick.campos@wizeline.com>
2020-04-19 18:04:15 -0500

    Add constants file

---
+++ bmi-calculator-flutter/lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+const kActiveCardColor = Color(0xFF1D1E33);
+const kInactiveCardColor = Color(0xff111328);
+const kCalculateButtonColor = Color(0xFFEB1555);
+const kNumberTextStyle = TextStyle(
+  fontWeight: FontWeight.w900,
+  fontSize: 50.0,
+);
