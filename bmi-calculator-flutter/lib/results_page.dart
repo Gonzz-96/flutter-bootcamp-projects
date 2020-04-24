c18bd44eb885a503edfbb871a79de144600d7a4e
Gonzalo Campos <erick.campos@wizeline.com>
2020-04-23 23:09:27 -0500

    Design results page.

--- bmi-calculator-flutter/lib/results_page.dart
+++ bmi-calculator-flutter/lib/results_page.dart
@@ -1,13 +1,82 @@
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
+
+import 'constants.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomLeft,
+              padding: EdgeInsets.only(
+                left: 20.0,
+              ),
+              child: Text(
+                'Your Results',
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: ReusableCard(
+              cardColor: kActiveCardColor,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      "STATUS",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.lightGreen),
+                    ),
+                    Text(
+                      "26.1",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 100.0,
+                      ),
+                    ),
+                    Text(
+                      "Here goes that shit",
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              color: kCalculateButtonColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: 80.0,
+              child: Center(
+                child: Text(
+                  'RE-CALCULATE',
+                  style: kLargeButtonTextStyle,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Text('hello'),
     );
   }
 }
