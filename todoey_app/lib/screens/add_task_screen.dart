a127ca76de2c1c2c9d3eea06e987f5234add79d4
Gonzalo Campos <erick.campos@wizeline.com>
2020-11-26 13:03:10 -0600

    Create and show AddTaskScreen in a modal.

---
+++ todoey_app/lib/screens/add_task_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.lightBlueAccent, fontSize: 25.0),
+            ),
+            TextField(
+              autofocus: true,
+              decoration: InputDecoration(),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            FlatButton(
+              onPressed: () {},
+              color: Colors.lightBlueAccent,
+              child: Text(
+                'Add',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
